fad8b9f6874897a934383528b54796d1e066d41b
shivam <shivam1705of@gmail.com>
2025-07-02 01:23:25 +0530

    resolved merge confilct

--- hello.cpp
+++ hello.cpp
@@ -4,16 +4,12 @@ int product(int a,int b){
     return a*b;
 }
 int main (){
-<<<<<<< HEAD
     // remem ber the anme he is kohn cena
-return 0;
-}
-=======
 
     cout<<"hello from main branch";
     cout<<"from feature 2";
     cout<<"hello from feaure 2";
-
+// so. my name is john cen a form seven heave that also i am king of this jungle 
 
 
 return 1;
@@ -21,4 +17,3 @@ return 1;
 int sum(int a){
     return a+1;
 }
->>>>>>> main
