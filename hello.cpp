2427b33134552a9b5fccc0637b6ba59bb6fdf33d
shivam <shivam1705of@gmail.com>
2025-07-02 01:08:18 +0530

    add:sum function and product

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std; 
-
+int product(int a,int b){
+    return a*b;
+}
 int main (){
     cout<<"from feature 2";
     cout<<"hello from feaure 2";
 return 1;
+}
+int sum(int a){
+    return a+1;
 }
